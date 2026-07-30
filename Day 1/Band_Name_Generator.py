ad8a9dcf8bb3d13d2d68f1534abd4131df7b012d
Zheel Nanda <65588774+Zheelncodeeasy@users.noreply.github.com>
2026-07-30 16:44:14 +0530

    Day 1 Completed

---
+++ Day 1/Band_Name_Generator.py
@@ -0,0 +1,10 @@
+print("Welcome to the Band Name Generator.")
+
+# Input your city name where youu grew up
+city_name = input("What's the name of the city you grew up in?\n")
+
+# Input your pet name
+pet_name = input("What's your pet name?\n")
+
+# Result - city name and pet name
+print("Your band name could be" + " " + city_name + " " + pet_name) # string concatenation
